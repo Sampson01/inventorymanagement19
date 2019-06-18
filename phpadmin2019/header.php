9a949ad2e1bde7d5b6652dca506c5c232b1988c5
Admin <admin@tests-MacBook-Pro-3.local>
2019-06-18 16:01:52 -0500

    Fixing databse requirements

--- phpadmin2019/header.php
+++ phpadmin2019/header.php
@@ -1,107 +1,38 @@
-<?php
-  session_start();
- ?>
-
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta name="description" content="This is an example of a meta description. Will show up in search results.">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Q's App</title>
+    <title></title>
 
   </head>
+  <body>
+
 
       <header>
-
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-
-        </script>
-
-        <style>
-        @import url('https://fonts.googleapis.com/css?family=Bree+Serif|Sedgwick+Ave+Display');
-        .container{
-          width:50%;
-
-        }
-        .form-signin,.form-control,button{
-          margin: 20px;
-          padding: 20px;
-        }
-        #head{
-        background-color: f8f8f8;
-        padding: 20px;
-        font-size:40px;
-        font-family: 'Bree Serif', serif;
-        }
-
-        </style>
-
-
-
-
-
+        <nav>
+          <a href="">
+            <img src="" alt="">
+          </a>
+          <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">About me</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
 
           <div>
-            <?php
-              if (isset($_SESSION['userID'])) {
-                echo '<form action="includes/logout.inc.php" method="post">
-                  <button type="submit" name="logout-submit">Logout</button>
-                  </form>';
-              }
-              else {
-                echo '<form action="includes/login.inc.php" method="post">
-            <!--
-                    <input type="text" name="mailuid" placeholder="Username/E-mail">
-                    <input type="password" name="pwd" placeholder="Password">
-                    <button type="submit" name="login-submit">Login</button>
-
-            -->
-                             <div id="head"><span class="glyphicon glyphicon-grain" style="color:green;"></span><span style="color:green;">Qs </span>Inventory<span style="color:green;">Management</span></div>
-
-                           <div class="container" id="loginDiv">
-
-                                 <form class="form-signin">
-                                   <h3 class="form-signin-heading">Member Login</h3>
-
-                                   <label for="uname" class="sr-only">Staff ID</label>
-                                   <input type="text" id="inputEmail" class="form-control" name="mailuid" placeholder="Staff ID" required autofocus="">
-
-                                   <label for="psw" class="sr-only">Password</label>
-                                   <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" name="psw" required autofocus="">
-                           <center>
+            <form action="includes/login.inc.php" method="post">
+              <input type="text" name="mailuid" placeholder="Username/E-mail">
+              <input type="password" name="pwd" placeholder="Password">
+              <button type="submit" name="login-submit">Login</button>
+            </form>
 
 
-                                   <!-- <div class="checkbox">
-                                     <label>
-                                       <input type="checkbox" checked="checked" name="remember"value="remember-me"> Remember me
-                                     </label>
-                                   </div> -->
-
-                                   <button type="submit" class="btn btn-lg btn-primary btn-block" name=login-submit action="includes/login.inc.php" method="post">LogIn</button>
-                           </center>
-                                 </form>
-
-                               </div>
-
-
-
-                  </form>
-                  <a href="signup.php" name="signup-submit">Signup</a>
-                  <form action="includes/logout.inc.php" method="post">
-                      <button type="submit" name="logout-submit">Logout</button>
-                    </form>';
-
-              }
-
-             ?>
-
-
-
+            <form action="includes/logout.inc.php" method="post">
+              <button type="submit" name="logout-submit">Logout</button>
+            </form>
           </div>
-
+        </nav>
       </header>
--- phpadmin2019/index.php
+++ phpadmin2019/index.php
@@ -3,31 +3,8 @@
 ?>
 
     <main>
-      <?php
-        if (isset($_SESSION['userID'])) {
-          echo "You are logged in!";
-        }
-        else {
-          echo "You are logged out!";
-        }
-       ?>
-
-
-       <body>
-
-          <nav>
-
-                  <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="product-entry.html">Product Entry</a></li>
-                    <li><a href="product-editor.html">Product Editor</a></li>
-                    <li><a href="billing.html">Billing</a></li>
-                  </ul>
-          </nav>
-
-
-       </body>
-
+      <p>You are logged out!</p>
+      <p>You are logged in!</p>
     </main>
 
 
